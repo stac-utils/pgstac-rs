5703233f860a6751da79ef98bd723fbcdeea81c3
Pete Gadomski <pete.gadomski@gmail.com>
2023-06-16 08:44:57 -0600

    Revert "refactor: make the client owned"
    This reverts commit 69ed1a90efcbaaa49e5c900e37bddd2f8d38c6c3.

--- pgstac-test/src/lib.rs
+++ pgstac-test/src/lib.rs
@@ -20,10 +20,9 @@ fn impl_pgstac_test(ast: ItemFn) -> TokenStream {
                 connection.await.unwrap()
             });
             let transaction = client.transaction().await.unwrap();
-            let client = Client::new(transaction);
+            let client = Client::new(&transaction);
             #ast
             #ident(&client).await;
-            let transaction = client.into_inner();
             transaction.rollback().await.unwrap();
         }
     };
